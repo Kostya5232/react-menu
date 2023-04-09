e4ebe9f48f3240b37232b88cacbe86a4f4d0e99d
Константин <kostyagreduagin.ru@gmail.com>
2023-04-09 23:28:53 +0400

    main

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React from 'react';
+import Menu from './components/Menu/Menu';
+import CalculatorComponents from './components/calculator/CalculatorComponents';
+import Graph2DComponent from './components/Graph2D/Graph2DComponent';
+import Graph3DComponent from './components/Graph3D/Graph3DComponent';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { showMenuItem: 'graph3D' };
+
+    this.showMenuItem('menu');
+    this.showMenuItem('calc');
+    this.showMenuItem('graph2D');
+    this.showMenuItem('graph3D');
+  }
+
+  showMenuItem(name) {
+    this.setState({ showMenuItem: name });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Menu showMenuItem={(name) => this.showMenuItem(name)} />
+        {this.state.showMenuItem === 'calc' ? (
+          <CalculatorComponents />
+        ) : this.state.showMenuItem === 'graph2D' ? (
+          <Graph2DComponent />
+        ) : this.state.showMenuItem === 'graph3D' ? (
+          <Graph3DComponent />
+        ) : (
+          <></>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
